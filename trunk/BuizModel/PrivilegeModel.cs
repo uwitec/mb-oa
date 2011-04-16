cc656cb94dd675d74964335548f2812f45c17a40
fromskyline <fromskyline@88bfa294-21a3-a380-c931-e81bd10a7773>
2011-04-16 15:09:34 +0000

    增加由模块功能树节点切换到操作表刷新

--- trunk/BuizModel/PrivilegeModel.cs
+++ trunk/BuizModel/PrivilegeModel.cs
@@ -14,7 +14,29 @@ namespace BuizModel
             using (MyDB mydb = new MyDB())
             {
                 return mydb.Privileges
-                    .Select(p => new { p.ID, p.resource.resourceName,p.resource.module.moduleName,p.privilegeName, p.privilegeCode, p.orderNO }) //快速匿名对象
+                    .Select(p => new { p.ID, p.resource.resourceName,p.resource.module.moduleName,p.privilegeName, p.privilegeCode,p.needAuth,p.isMenuEntry,p.createdTime, p.orderNO }) //快速匿名对象
+                    //.Select(p => p) //是否可以通过判断类型来取属性，例如只取简单类型的属性
+                    .ToArray();
+            }
+        }
+
+        public static object[] getListByModule(string ModuleID)
+        {
+            using (MyDB mydb = new MyDB())
+            {
+                return mydb.Privileges.Where(p=>p.resource.module.ID.Equals(ModuleID))
+                    .Select(p => new { p.ID, p.resource.resourceName, p.resource.module.moduleName, p.privilegeName, p.privilegeCode, p.needAuth, p.isMenuEntry, p.createdTime, p.orderNO }) //快速匿名对象
+                    //.Select(p => p) //是否可以通过判断类型来取属性，例如只取简单类型的属性
+                    .ToArray();
+            }
+        }
+
+        public static object[] getListByResource(string ResourceID)
+        {
+            using (MyDB mydb = new MyDB())
+            {
+                return mydb.Privileges.Where(p=>p.resource.ID.Equals(ResourceID))
+                    .Select(p => new { p.ID, p.resource.resourceName, p.resource.module.moduleName, p.privilegeName, p.privilegeCode, p.needAuth, p.isMenuEntry, p.createdTime, p.orderNO }) //快速匿名对象
                     //.Select(p => p) //是否可以通过判断类型来取属性，例如只取简单类型的属性
                     .ToArray();
             }
