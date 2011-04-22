1969800e035c6452fe22de8a863a02ed3b829f0e
fromskyline <fromskyline@88bfa294-21a3-a380-c931-e81bd10a7773>
2011-04-22 04:37:21 +0000

    用角色权限参数实现分级管理

---
+++ trunk/TestProject/SendEmail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Mail;
+using System.Net;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class SendEmail
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                MailMessage mailObj = new MailMessage();
+                mailObj.From = new MailAddress("chen@manbuit.com");
+                mailObj.To.Add("437968474@qq.com");
+                mailObj.Subject = "主题";
+                mailObj.Body = "内容";
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.exmail.qq.com";
+                smtp.Port = 465;
+
+                //smtp.UseDefaultCredentials = true;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential("chen@manbuit.com", "971023chw");
+
+                smtp.Send(mailObj);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
