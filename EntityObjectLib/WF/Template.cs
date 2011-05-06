216bd6f548ac33023498d3501b1c58fd39345b71
fromskyline <fromskyline@88bfa294-21a3-a380-c931-e81bd10a7773>
2011-05-06 15:00:34 +0000

    增加工作流定义相关实体类

---
+++ EntityObjectLib/WF/Template.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityObjectLib.WF
+{
+    /// <summary>
+    /// 工作流模板
+    /// </summary>
+    public partial class Template
+    {
+        [Key]
+        public string ID { get; set; }
+
+        /// <summary>
+        /// 模板名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 业务类
+        /// </summary>
+        [NotMapped]
+        public IWFBuiz BuizType { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public virtual User Creator { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+
+
+}
+
+namespace EntityObjectLib
+{
+    public partial class User
+    {
+        /// <summary>
+        /// 用户创建的工作流模板
+        /// </summary>
+        public virtual ICollection<EntityObjectLib.WF.Template> CreateWFTs { get; set; }
+    }
+}
