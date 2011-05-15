b78a6566ad5e568a9c9fe7d579a46dda1fd66406
fromskyline <fromskyline@88bfa294-21a3-a380-c931-e81bd10a7773>
2011-05-15 11:08:27 +0000

    建模操作模型

--- trunk/BuizWeb/Content/canvas/WFModel.js
+++ trunk/BuizWeb/Content/canvas/WFModel.js
@@ -13,7 +13,7 @@ nodes: [
 { ID: '6', name: '人工处理', type: 'Handle', position: { x: 110, y: 37} },
 { ID: '7', name: '结束节点', type: 'Finish', position: { x: 66, y: 88} }
 ],
-Lines: [
+lines: [
 { from: '1', to: '2', middlePoint: { x: 11, y: 11} },
 { from: '2', to: '3', middlePoint: { x: 11, y: 11} },
 { from: '2', to: '4', middlePoint: { x: 11, y: 11} },
@@ -72,8 +72,8 @@ function WFGraph(config) {
         this.ctx.drawLineWithArrow({ x: 0, y: -10}, { x: 0, y: 45 });
 
         var n;
-        for (n in this.data.Lines) {
-            this.drawLine(this.data.Lines[n]);
+        for (n in this.data.lines) {
+            this.drawLine(this.data.lines[n]);
         }
 
         for (n in this.data.nodes) {
@@ -83,7 +83,7 @@ function WFGraph(config) {
         //alert('dddd');
     }
 
-    this.drawLine = function (line) {
+    this.getLine = function (line) {
         var start = {}, end = {}, n;
 
         // 找当前连线相关的头尾节点
@@ -97,12 +97,18 @@ function WFGraph(config) {
         }
 
         var dis = this.ctx.distance(start, end);
+        var lineBegin = { x: end.x + (start.x - end.x) * (dis - 20) / dis, y: end.y + (start.y - end.y) * (dis - 20) / dis }; //起点让20px
+        var lineEnd = { x: start.x + (end.x - start.x) * (dis - 20) / dis, y: start.y + (end.y - start.y) * (dis - 20) / dis }; //终点让30px
+
+        return { begin: lineBegin, end: lineEnd };
+    }
+
+    this.drawLine = function (line) {
+        var l = this.getLine(line);
 
         // 因为节点图片本身点据一定空间,连线要让开这部分空间,因此需要重新测算起点和终点,具体大小应该根据图片自动设置,以后改进
-        this.ctx.drawLineWithArrow(
-                    { x: end.x + (start.x - end.x) * (dis - 20) / dis, y: end.y + (start.y - end.y) * (dis - 20) / dis }, //起点让20px
-                    {x: start.x + (end.x - start.x) * (dis - 20) / dis, y: start.y + (end.y - start.y) * (dis - 20) / dis} //终点让30px
-                );
+        this.ctx.drawLineWithArrow(l.begin, l.end);
+        this.drawText(line.name, (l.begin.x + l.end.x) / 2, (l.begin.y + l.end.y) / 2);
     }
 
     this.drawNode = function (n) {
@@ -119,10 +125,6 @@ function WFGraph(config) {
         this.ctx.restore();
     }
 
-    //
-    this.selected = null;
-    this.operateState = null; // moveCanvas
-
     this.tranp = function (point) {
         return {
             x: point.x - transXY.x - Ext.fly(this.ctx.canvas).getOffsetsTo(document.body)[0] + Ext.fly(document.body).getScroll().left,
@@ -130,86 +132,230 @@ function WFGraph(config) {
         }
     }
 
-    var canvasMove = function (event) {
-        Ext.fly(this.ctx.canvas).setStyle('cursor', 'move');
-        if (this.operateState) {
-            transXY = { x: transXY.x + event.x - this.operateState.x, y: transXY.y - event.y + this.operateState.y };
-            this.redrawAll();
-            this.operateState = { x: event.x, y: event.y };
-        }
-    }
-
-    var canvasMouseDownHandler = function (event) {
-        //event = event.browserEvent;  //使用了normalized: false,不需要转化
+    this.captureNode = function (event) {
+        var node = null;
         for (var n in this.data.nodes) {
+            // 如果选中节点,注册移动事件处理为节点移动
             if (this.ctx.distance(this.data.nodes[n].position, this.tranp({ x: event.x, y: event.y })) < 16) {
-                this.selected = n;
-
-                //this.ctx.canvas.onmousemove = function (scope) { return function (event) { return scope.canvasMouseMoveHandler.call(scope, event); } } (this);
-                Ext.fly(this.ctx.canvas).on('mousemove', canvasMouseMoveHandler, this, { normalized: false });  // 上面也可以用
-
-                //this.ctx.canvas.onmouseup = function (scope) { return function (event) { return scope.canvasMouseUpHandler.call(scope, event); } } (this);
-                Ext.fly(this.ctx.canvas).on('mouseup', canvasMouseUpHandler, this);  // 上面也可以用
-
-                return;
-            }
-        }
-
-        this.operateState = { x: event.x, y: event.y }; //
-        
-        Ext.fly(this.ctx.canvas).on('mousemove', canvasMove, this, { normalized: false });  // 上面也可以用
-        Ext.fly(this.ctx.canvas).on('mouseup', function () { this.operateState = null; Ext.fly(this.ctx.canvas).un('mousemove', canvasMove); }, this, { normalized: false });  // 上面也可以用
-    }
-
-    var canvasMouseUpHandler = function (event) {
-        this.selected = null;
-        Ext.fly(this.ctx.canvas).un('mousemove', canvasMouseMoveHandler);
-        Ext.fly(this.ctx.canvas).un('mouseup', canvasMouseUpHandler);
-        //this.ctx.canvas.onmousemove = this.ctx.canvas.onmouseup = null;
-        
-    }
-
-    var canvasMouseMoveHandler = function (event) {
-        //event = event.browserEvent;
-        if (this.selected) {
-            this.data.nodes[this.selected].position = this.tranp({ x: event.x, y: event.y });
-            this.redrawAll();
-        }
-    }
-
-    var canvasDblClick = function (event) {
-        for (var n in this.data.nodes) {
-            if (this.ctx.distance(this.data.nodes[n].position, this.tranp({ x: event.x, y: event.y })) < 16) {
-                this.selected = n;
+                node = this.data.nodes[n]; // 选中的节点,处理后直接返回
                 break;
             }
         }
-        alert(this.selected);
-        this.selected = null;
+        return node;
     }
 
+    this.captureLine = function (event) {
+        var line = null;
+        for (var l in this.data.lines) {
+            // 如果选中节点,注册移动事件处理为节点移动
+            if (this.ctx.minDistance(this.tranp({ x: event.x, y: event.y }), this.getLine(this.data.lines[l])) < 6) {
+                line = this.data.lines[l];
+                break;
+            }
+        }
+        return line;
+    }
 
-    Ext.fly(this.ctx.canvas).on( // 注册mousedown事件响应
-        'mousedown',
-        canvasMouseDownHandler,
-        this,
-        { normalized: false }
-    );
+    // select 操作模式封装,处理节点移动,视口移动,双击节点事件
+    var selectOperateState = {
+        mode: 'select',
+        listeners: {
+            mousedown: function (event) {
+                //event = event.browserEvent;  //使用了normalized: false,不需要转化
+                var n = this.captureNode(event);
+                if (n) {
+                    this.operateState.params = { selected: n };
+                    Ext.fly(this.ctx.canvas).on('mousemove',
+                            function (event) {
+                                //event = event.browserEvent;
+                                if (this.operateState.params && this.operateState.params.selected) {
+                                    this.operateState.params.selected.position = this.tranp({ x: event.x, y: event.y });
+                                    this.redrawAll();
+                                }
+                            }, // function (event) 
+                            this,
+                            { normalized: false }
+                        ); //Ext.fly(this.ctx.canvas).on
+                }
+            },
+            mousemove: null,
+            mouseup: function (event) {
+                this.resetOperateState();
+            },
+            dblclick: function (event) {
+                var n = this.captureNode(event) || this.captureLine(event);
+                if (n)
+                    alert(n.name);
+            }
+        },
+        params: null
+    };
 
-    Ext.fly(this.ctx.canvas).on( // 注册mousedown事件响应
-        'dblclick',
-        canvasDblClick,
-        this,
-        { normalized: false }
-    );
+    var moveOperateState = {
+        mode: 'move',
+        listeners: {
+            mousedown: function (event) {
+                //event = event.browserEvent;  //使用了normalized: false,不需要转化
+                this.operateState.params = { x: event.x, y: event.y }; //
+                Ext.fly(this.ctx.canvas).on('mousemove',
+                    function (event) { //////////////////////////////
+                        if (this.operateState.params && this.operateState.params.x) {
+                            transXY = { x: transXY.x + event.x - this.operateState.params.x, y: transXY.y - event.y + this.operateState.params.y };
+                            this.redrawAll();
+                            this.operateState.params = { x: event.x, y: event.y };
+                        }
+                    }, ///////////////////////////////////////////////////////
+                    this,
+                    { normalized: false }
+                );
+            },
+            mousemove: null,
+            mouseup: function (event) {
+                this.operateState.params = null;
+            },
+            dblclick: null
+        },
+        params: null
+    };
+
+    var newHandleOperateState = {
+        mode: 'newHandle',
+        listeners: {
+            mousedown: function (event) {
+                this.data.nodes.push({ ID: GUID(), name: 'new', type: 'Handle', position: this.tranp(event) });
+                this.redrawAll();
+            },
+            mousemove: null,
+            mouseup: null,
+            dblclick: null
+        },
+        params: null
+    };
+
+    var newSplitOperateState = {
+        mode: 'newSplit',
+        listeners: {
+            mousedown: function (event) {
+                this.data.nodes.push({ ID: GUID(), name: 'new', type: 'XORSplit', position: this.tranp(event) });
+                this.redrawAll();
+            },
+            mousemove: null,
+            mouseup: null,
+            dblclick: null
+        },
+        params: null
+    };
+
+    var newTransOperateState = {
+        mode: 'newTrans',
+        listeners: {
+            click: function (event) {
+                var n = this.captureNode(event);
+                if (this.operateState.params && this.operateState.params.preNode) {
+                    if (n) {
+                        if (n.ID != this.operateState.params.preNode.ID) { //还有一种情况,如果连接已经存在
+                            this.data.lines.push({ ID: GUID(), from: this.operateState.params.preNode.ID, to: n.ID, name: '未命名' });
+                            this.operateState.params = { preNode: null };
+                            this.redrawAll();
+                        }
+                        else {
+                            alert('两端的节点必须是不同的!');
+                        }
+                    }
+                }
+                else {
+                    this.operateState.params = { preNode: n };
+                }
+            },
+            mousemove: null,
+            mouseup: null,
+            dblclick: null
+        },
+        params: null
+    };
+
+    var deleteOperateState = {
+        mode: 'delete',
+        listeners: {
+            click: function (event) {
+                var n = this.captureNode(event);
+                if (n && confirm("确认删除该节点及其所有连接吗?")) {
+                    for (var line in this.data.lines) {
+                        if (line.from == n.ID || line.to == n.ID) {
+                            var b = this.data.lines.indexOf(line);
+                            if (b >= 0) {
+                                this.data.nodes.splice(b, 1);
+                            }
+                        }  // end of if
+                    }
+                    var a = this.data.nodes.indexOf(n);
+                    if (a >= 0) {
+                        this.data.nodes.splice(a, 1);
+                    }
+                    this.redrawAll();
+                }
+            }
+        },
+        params: null
+    };
+
+    // 可用的操作状态列表
+    var operateStates = {
+        select: selectOperateState,
+        move: moveOperateState,
+        newHandle: newHandleOperateState,
+        newSplit: newSplitOperateState,
+        newTrans: newTransOperateState,
+        'delete': deleteOperateState
+    };
+
+    // 操作状态的模式及参数
+    this.operateState = { };
+
+    // 重置操作状态为初始,初始为select
+    this.resetOperateState = function () {
+        this.setOperateState("select");
+    }
+
+    // 设置操作状态
+    this.setOperateState = function (model) {
+        Ext.fly(this.ctx.canvas).clearListeners(); //清除所有事件监听
+
+        this.operateState = operateStates[model];
+        for (var eventName in this.operateState.listeners) {
+            if (this.operateState.listeners[eventName]) {// 注册mousedown事件响应
+                Ext.fly(this.ctx.canvas).on(eventName, this.operateState.listeners[eventName], this, { normalized: false });
+            }
+        }
+    }
+
+    this.resetOperateState();
 
     this.fullCanvas();
 }
 
 
+
 // 扩展方法,计算两点间的距离
 CanvasRenderingContext2D.prototype.distance = function (start, end) {
     return Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2));
+}
+
+// 扩展方法,计算点到线段的距离,如果垂足在线段外,则取点到线段端点距离的最小值
+// point: {x:10,y:10} , segment: {begin: {x:10,y:10},end: {x:10,y:10}}
+// 假设线外点为P,线段起点为B,线段终点为E
+CanvasRenderingContext2D.prototype.minDistance = function (point, segment) {
+    var PB = this.distance(point, segment.begin);
+    var PE = this.distance(point, segment.end);
+    var BE = this.distance(segment.begin, segment.end);
+
+    if (BE * BE + PB * PB <= PE * PE)  //PBE是钝角
+        return PB;
+    if (BE * BE + PE * PE <= PB * PB)
+        return PE;
+
+    var p = (PB + PE + BE) / 2;
+    return Math.sqrt(p * (p - PB) * (p - PE) * (p - BE)) * 2 / BE; //海伦公式
 }
 
 // 扩展方法,画终点是箭头的直线
